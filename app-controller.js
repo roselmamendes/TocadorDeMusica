ef4ddbff461dd5a5e92d8ab8384eba3cc617d059
Roselma Mendes <roselma.mendes@gmail.com>
2017-03-26 12:05:56 -0300

    WIP

--- app-controller.js
+++ app-controller.js
@@ -2,10 +2,15 @@ angular.module('App').
     controller('AppController', function(queueService){
 	var scope = this;
 	scope.queue = [];
+    scope.music = "teste";
 	scope.addSong = function(music){
 	   queueService.addSong(music);
 	};
 
+    scope.todos = [
+        {id:"teste", src: "teste.mp3"}
+    ];
+    
 	scope.updateMusicList = function(){
 	    scope.queue = queueService.getAllSongs();
 	}
